93eb7341824d68d8c786d897b26d3e40d5d9a8d7
Prithvi <prithvikhanna10@gmail.com>
2020-06-29 20:35:23 +0530

    final

---
+++ src/main/java/DemoList.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+
+public class DemoList {
+    ArrayList<Integer> al = new ArrayList<Integer>();
+
+    void addelement(int element)
+    {
+        al.add(element);
+    }
+    int getElement(int position)
+    {
+        return al.get(position);
+    }
+    int position(int element)
+    {
+        return al.indexOf(element);
+    }
+    void remove(int position)
+    {
+        al.remove(al.get(position));
+    }
+    void removelast()
+    {
+        al.remove(al.get(al.size()-1));
+    }
+    void print()
+    {
+        System.out.print(al);
+        System.out.println();
+    }
+}
